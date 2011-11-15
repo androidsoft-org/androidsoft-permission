2bcdeec3f1fc02f80d78b63185bf72498f4ea478
androidsoft.org@gmail.com <androidsoft.org@gmail.com@0b6fd139-98b2-6a13-753f-527d200dc8eb>
2011-11-15 01:14:11 +0000

    new icons, toggle sort, market link, credits release 0.9.1

--- trunk/permission/src/org/androidsoft/app/permission/ui/ApplicationFragment.java
+++ trunk/permission/src/org/androidsoft/app/permission/ui/ApplicationFragment.java
@@ -18,17 +18,21 @@ import org.androidsoft.app.permission.ui.widget.PermissionExpandableListAdapter;
 import org.androidsoft.app.permission.model.PermissionGroup;
 import org.androidsoft.app.permission.service.PermissionService;
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ExpandableListView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import java.util.List;
 import org.androidsoft.app.permission.R;
@@ -37,21 +41,20 @@ import org.androidsoft.app.permission.R;
  *
  * @author pierre
  */
-public class ApplicationFragment extends Fragment
+public class ApplicationFragment extends Fragment implements View.OnClickListener
 {
     private static final String TAG = "androidsoft";
     private TextView mName;
     private ImageView mIcon;
-    private TextView mPackageName;
-    private TextView mVersion;
+    private TextView mTvPackageName;
+    private TextView mTvVersion;
     private ExpandableListView mPermissions;
+    private LinearLayout mNoPermissionLayout;
+    private Button mButtonMarket;
+    private String mPackageName;
     
     /**
-     * 
-     * @param inflater
-     * @param container
-     * @param savedInstanceState
-     * @return
+     * {@inheritDoc }
      */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -62,9 +65,12 @@ public class ApplicationFragment extends Fragment
         
         mName = (TextView) v.findViewById(R.id.name );
         mIcon = (ImageView) v.findViewById(R.id.icon );
-        mPackageName = (TextView) v.findViewById(R.id.package_name );
-        mVersion = (TextView) v.findViewById(R.id.version );
-        mPermissions = (ExpandableListView) v.findViewById(R.id.permissions_list);        
+        mTvPackageName = (TextView) v.findViewById(R.id.package_name );
+        mTvVersion = (TextView) v.findViewById(R.id.version );
+        mPermissions = (ExpandableListView) v.findViewById(R.id.permissions_list);  
+        mNoPermissionLayout = (LinearLayout) v.findViewById(R.id.layout_no_permission);
+        mButtonMarket = (Button) v.findViewById( R.id.button_market);
+        mButtonMarket.setOnClickListener( this );
         return v;
     }
     
@@ -72,12 +78,13 @@ public class ApplicationFragment extends Fragment
     {
         try
         {
+            mPackageName = packageName;
             PackageManager pm = activity.getPackageManager();
             PackageInfo pi = pm.getPackageInfo(packageName, PackageManager.GET_PERMISSIONS );
             mName.setText(pi.applicationInfo.loadLabel(pm).toString());
             mIcon.setImageDrawable( pi.applicationInfo.loadIcon(pm));
-            mPackageName.setText(packageName);
-            mVersion.setText(pi.versionName);
+            mTvPackageName.setText(packageName);
+            mTvVersion.setText(pi.versionName);
             List<PermissionGroup> listGroups = PermissionService.getPermissions( pi.requestedPermissions , pm );
             PermissionExpandableListAdapter adapter = new PermissionExpandableListAdapter( getActivity(), listGroups );  
             mPermissions.setAdapter(adapter);
@@ -85,11 +92,30 @@ public class ApplicationFragment extends Fragment
             {
                 mPermissions.expandGroup(i);
             }
+            if( listGroups.isEmpty() )
+            {
+                mNoPermissionLayout.setVisibility(View.VISIBLE);
+            }
         }
         catch (NameNotFoundException ex)
         {
             Log.e( TAG , "Package name not found : " + packageName );
         }
+    }
+
+    public void onClick(View view)
+    {
+        if( view == mButtonMarket )
+        {
+            openMarket();
+        }
+    }
+
+    private void openMarket()
+    {
+        String uri = "market://details?id=" + mPackageName;
+        Intent goToMarket = new Intent(Intent.ACTION_VIEW,Uri.parse( uri ));
+        startActivity(goToMarket);
     }
     
 }
